41b6d07033bc2a3d1c8590894ce4658bba03fd7f
Lautaro Oscar Arellano <arellanolautaro10@gmail.com>
2023-01-20 20:18:02 -0300

    20-1-2023

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,6 @@
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
-function mostrar()
-{
-	alert("probando con git hub ");
+function mostrar() {
+  alert("Esto funciona de maravilla");
 }
-
